bc69de2f916d8e67f57e46121575a015e2e11087
Justas Balcas <juztas@gmail.com>
2024-07-15 09:03:56 -0500

    Add feature to allow execute commands (e.g. ping, traceroute)

--- plugins/modules/cisconx9_command.py
+++ plugins/modules/cisconx9_command.py
@@ -1,8 +1,18 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""Cisco NX9 Command execute
+Copyright: Contributors to the SENSE Project
+GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-# Copyright: Contributors to the Ansible project
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+Title                   : sdn-sense/sense-cisconx9-collection
+Author                  : Justas Balcas
+Email                   : juztas (at) gmail.com
+@Copyright              : General Public License v3.0+
+Date                    : 2024/07/15
+"""
+from __future__ import (absolute_import, division, print_function)
+
+__metaclass__ = type
 
 import time
 from ansible.module_utils.basic import AnsibleModule
@@ -18,19 +28,28 @@ display = Display()
 
 @functionwrapper
 def toLines(stdout):
+    """stdout to list lines, split by \n character"""
     for item in stdout:
         if isinstance(item, string_types):
             item = str(item).split('\n')
         yield item
 
 @functionwrapper
-def parse_commands(module, warnings):
-    command = ComplexList({'command': {'key':True}, 'prompt': {}, 'answer':{}}, module)
-    commands = command(module.params['commands'])
+def parse_commands(module, _warnings):
+    """Parse commands"""
+    command = ComplexList({'command': {'key': True}, 'prompt': {}, 'answer': {}}, module)
+    if module.params.get("src", ""):
+        # Load src file
+        with open(module.params["src"], encoding="utf-8") as fd:
+            cmds = fd.readlines()
+            # if cmd starts with comment, ignore:
+            cmds = [cmd for cmd in cmds if not cmd.startswith("#")]
+            commands = command(cmds)
+    elif module.params["commands"]:
+        commands = command(module.params["commands"])
+
     for _index, item in enumerate(commands):
-        if module.check_mode and not item['command'].startswith('show'):
-            warnings.append('only show commands are supported when using check mode, not executing `%s`' % item['command'])
-        elif item['command'].startswith('conf'):
+        if item['command'].startswith('conf'):
             module.fail_json(msg='cisconx9_command does not support running config mode commands.  Please use cisconx9_config instead')
     return commands
 
@@ -38,17 +57,20 @@ def parse_commands(module, warnings):
 def main():
     """main entry point for module execution
     """
-
     argument_spec = {
-        'commands': {'type': 'list', 'required':True},
-        'wait_for': {'type':'list', 'elements': 'str'},
-        'match': {'default':'all', 'choices': ['all', 'any']},
-        'retries': {'default':10, 'type': 'int'},
+        'lines': {'aliases': ['commands'], 'type': 'list'},
+        'src': {'type': 'path'},
+        'wait_for': {'type': 'list', 'elements': 'str'},
+        'match': {'default': 'all', 'choices': ['all', 'any']},
+        'retries': {'default': 10, 'type': 'int'},
         'interval': {'default': 1, 'type': 'int'}}
 
     argument_spec.update(cisconx9_argument_spec)
 
+    mutually_exclusive = [("lines", "src")]
+
     module = AnsibleModule(argument_spec=argument_spec,
+                           mutually_exclusive=mutually_exclusive,
                            supports_check_mode=True)
 
     result = {'changed': False}
@@ -64,7 +86,7 @@ def main():
     retries = module.params['retries']
     interval = module.params['interval']
     match = module.params['match']
-
+    responses = None
     while retries > 0:
         responses = run_commands(module, commands)
 
