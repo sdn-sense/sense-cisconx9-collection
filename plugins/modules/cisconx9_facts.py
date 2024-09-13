a36304cc645c6100fe662bacbaad934d650f11d5
Justas Balcas <juztas@gmail.com>
2024-09-13 07:36:38 -0500

    Reference before use

--- plugins/modules/cisconx9_facts.py
+++ plugins/modules/cisconx9_facts.py
@@ -89,7 +89,7 @@ class Interfaces(FactsBase):
     @staticmethod
     def _validate(indata, keys):
         """Validate response and preset default values"""
-        if isinstance(tmpval, dict):
+        if isinstance(indata, dict):
             display.vvv(f"Data is not a dictionary: {indata}. Rewrite as dict")
             indata = {}
         tmpval = indata
